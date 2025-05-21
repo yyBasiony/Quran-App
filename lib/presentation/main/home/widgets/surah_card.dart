1f1740ccf9bdad1422801288b45b8f1c1055f376
yasmine <iayasmine34@gmail.com>
2025-05-21 18:19:48 +0300

    update surah card

--- lib/presentation/main/home/widgets/surah_card.dart
+++ lib/presentation/main/home/widgets/surah_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/presentation/resources/app_colors.dart';
 import '../../../../models/surah_model.dart';
 import '../surah_details_screen.dart';
 
@@ -12,17 +13,11 @@ class SurahCard extends StatelessWidget {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
 
-    final avatarBgColor = theme.brightness == Brightness.dark
-        ? Colors.green.shade700  
-        : theme.colorScheme.primary; 
-
-    final avatarTextColor = theme.brightness == Brightness.dark
-        ? Colors.white70
-        : Colors.white;
+    final avatarTextColor = theme.brightness == Brightness.dark ? Colors.white70 : Colors.white;
 
     return ListTile(
       leading: CircleAvatar(
-        backgroundColor: avatarBgColor,
+        backgroundColor: AppColors.primary,
         child: Text(
           surah.number.toString(),
           style: TextStyle(color: avatarTextColor),
