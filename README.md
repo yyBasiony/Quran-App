8ee559846e39c1d5224783b0945e7ed8abeca484
yasmine <iayasmine34@gmail.com>
2025-06-30 03:28:20 +0300

    update image path

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # quran_app
 
-![ splash screen](https://github.com/yyBasiony/Quran-App/blob/main/assets/spla.jpg)
-![ start screen](https://github.com/yyBasiony/Quran-App/blob/main/assets/start.jpg)
-![ prayer time](https://github.com/yyBasiony/Quran-App/blob/main/assets/time_p.jpg)
-![ prayer time city](https://github.com/yyBasiony/Quran-App/blob/main/assets/select_cit.jpg)
-![dark prayer time](https://github.com/yyBasiony/Quran-App/blob/main/assets/dark_p_tim.jpg)
-![ surah list](https://github.com/yyBasiony/Quran-App/blob/main/assets/surah_list.jpg)
-![dark surah list](https://github.com/yyBasiony/Quran-App/blob/main/assets/dark_list.jpg)
-![ surah](https://github.com/yyBasiony/Quran-App/blob/main/assets/surah_text.jpg)
-![dark surah](https://github.com/yyBasiony/Quran-App/blob/main/assets/dark_text.jpg)
-![ surah audio](https://github.com/yyBasiony/Quran-App/blob/main/assets/audio.jpg)
-![dark surah audio](https://github.com/yyBasiony/Quran-App/blob/main/assets/audio_dark.jpg)
-![ ayah search](https://github.com/yyBasiony/Quran-App/blob/main/assets/sera.jpg)
-![ setting screen](https://github.com/yyBasiony/Quran-App/blob/main/assets/setting.jpg)
+![ splash screen](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/spla.jpg)
+![ start screen](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/start.jpg)
+![ prayer time](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/time_p.jpg)
+![ prayer time city](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/select_cit.jpg)
+![dark prayer time](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/dark_p_tim.jpg)
+![ surah list](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/surah_list.jpg)
+![dark surah list](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/dark_list.jpg)
+![ surah](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/surah_text.jpg)
+![dark surah](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/dark_text.jpg)
+![ surah audio](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/audio.jpg)
+![dark surah audio](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/audio_dark.jpg)
+![ ayah search](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/sera.jpg)
+![ setting screen](https://github.com/yyBasiony/Quran-App/blob/main/assets/images/setting.jpg)
 
 
 
--- lib/presentation/screens/surah/surah_details_screen.dart
+++ lib/presentation/screens/surah/surah_details_screen.dart
@@ -47,50 +47,49 @@ class _SurahDetailScreenState extends State<SurahDetailScreen> {
     final theme = Theme.of(context);
 
     return Scaffold(
-      backgroundColor: theme.scaffoldBackgroundColor,
-      appBar: AppBar(
-          backgroundColor: theme.primaryColor, title: Text(widget.surahName, style: theme.textTheme.titleLarge?.copyWith(color: Colors.white))),
-      body: Consumer<SurahDetailProvider>(
-        builder: (context, provider, child) {
-          if (provider.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return Column(children: [
-            Container(
-                margin: EdgeInsets.symmetric(vertical: 16.h),
-                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
-                decoration: BoxDecoration(
-                  border: Border.all(color:theme.primaryColor, width: 1.5.w),
-                  borderRadius: BorderRadius.circular(16.r),
-                ),
-                child: Text(widget.surahName,
-                    style: TextStyle(
-                      fontSize: 24.sp,
-                      fontWeight: FontWeight.bold,
-                      color: theme.primaryColor,
-                      fontFamily: 'Uthmani',
-                    ))),
-            Expanded(
-                child: SingleChildScrollView(
-                    padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
-                    child: RichText(
-                        textAlign: TextAlign.center,
-                        textDirection: TextDirection.rtl,
-                        text: TextSpan(
-                          style: TextStyle(
-                            fontSize: 22.sp,
-                            fontFamily: 'Uthmani',
-                            color: theme.textTheme.bodyLarge?.color,
-                          ),
-                          children: provider.ayahs.map((ayah) {
-                            return TextSpan(text: '${ayah.text} ﴿${ayah.number}﴾ ');
-                          }).toList(),
-                        ))))
-          ]);
-        },
-      ),
-      bottomNavigationBar: Consumer<SurahDetailProvider>(
-        builder: (context, provider, child) {
+        backgroundColor: theme.scaffoldBackgroundColor,
+        appBar: AppBar(
+            backgroundColor: theme.primaryColor, title: Text(widget.surahName, style: theme.textTheme.titleLarge?.copyWith(color: Colors.white))),
+        body: Consumer<SurahDetailProvider>(
+          builder: (context, provider, child) {
+            if (provider.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return Column(children: [
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 16.h),
+                  padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: theme.primaryColor, width: 1.5.w),
+                    borderRadius: BorderRadius.circular(16.r),
+                  ),
+                  child: Text(widget.surahName,
+                      style: TextStyle(
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.bold,
+                        color: theme.primaryColor,
+                        fontFamily: 'Uthmani',
+                      ))),
+              Expanded(
+                  child: SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+                      child: RichText(
+                          textAlign: TextAlign.center,
+                          textDirection: TextDirection.rtl,
+                          text: TextSpan(
+                            style: TextStyle(
+                              fontSize: 22.sp,
+                              fontFamily: 'Uthmani',
+                              color: theme.textTheme.bodyLarge?.color,
+                            ),
+                            children: provider.ayahs.map((ayah) {
+                              return TextSpan(text: '${ayah.text} ﴿${ayah.number}﴾ ');
+                            }).toList(),
+                          ))))
+            ]);
+          },
+        ),
+        bottomNavigationBar: Consumer<SurahDetailProvider>(builder: (context, provider, child) {
           return BottomAppBar(
               elevation: 10,
               color: theme.cardColor,
@@ -122,8 +121,6 @@ class _SurahDetailScreenState extends State<SurahDetailScreen> {
                       }).toList(),
                     )
                   ])));
-        },
-      ),
-    );
+        }));
   }
 }
